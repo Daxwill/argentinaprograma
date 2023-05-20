3e8d14369f01883a13f1e2e115a3bdbc47cb973c
Daxwill <74215083+Daxwill@users.noreply.github.com>
2023-05-20 11:46:47 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,8 @@
+function conversionGrados(fahrenheit) {
+    return (fahrenheit - 32) * (5 / 9);
+}
+
+console.log("Para la pregunta a es: " + conversionGrados(50));
+// a) 10 C°
+console.log("Para la pregunta b es: " + conversionGrados(75));
+// b) 23.8889 C°
