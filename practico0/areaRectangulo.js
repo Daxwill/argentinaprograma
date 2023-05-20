59b76de7c1299fe04496cfe8543aad19ebc087df
Daxwill <74215083+Daxwill@users.noreply.github.com>
2023-05-20 11:49:12 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,8 @@
+function areaRectangulo(base, altura) {
+    return base * altura;
+}
+
+console.log("la respuesta de la a es: " + areaRectangulo(5, 3));
+//a) 15
+console.log("la respuesta de la b es: " + areaRectangulo(12.5, 5.3));
+//b) 66.25
---
+++ practico0/promedio3.js
@@ -0,0 +1,10 @@
+function promedio3(x, y, z) {
+    return (x + y + z) / 3;
+}
+
+console.log("La respuesta de a es: " + promedio3(5, 7, 9));
+// a) 7
+console.log("La respuesta de b es: " + promedio3(-2, -3, -8));
+// b) -4.333
+console.log("La respuesta de c es: " + promedio3(-1.2, 5.3, -3.3));
+// c) 0.2666
