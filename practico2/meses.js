b57274395e97daf96c7a7cc84db4efd506a2066b
Daxwill <74215083+Daxwill@users.noreply.github.com>
2023-06-13 20:15:44 -0300

    subi ejercicio 2

---
+++ practico2/meses.js
@@ -0,0 +1,8 @@
+const readlineSync = require('readline-sync');
+let mesUsuario = readlineSync.question("Ingrese el numero del mes: ");
+
+const ARRAYMESES = [{ mes: "enero", dias: 31 }, { mes: "febrero", dias: 28 }, { mes: "marzo", dias: 31 }, { mes: "abril", dias: 30 },
+{ mes: "mayo", dias: 31 }, { mes: "junio", dias: 30 }, { mes: "julio", dias: 31 }, { mes: "agosto", dias: 31 },
+{ mes: "septiembre", dias: 30 }, { mes: "octubre", dias: 31 }, { mes: "noviembre", dias: 30 }, { mes: "diciembre", dias: 31 }];
+
+console.log("La cantidad de dias del mes de " + ARRAYMESES[mesUsuario - 1].mes + " es " + ARRAYMESES[mesUsuario - 1].dias);
